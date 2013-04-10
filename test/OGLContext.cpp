3e49fee8ea2f85e9847fddc75c035aee75e52d51
yozhijk <dmitry.a.kozlov@gmail.com>
2013-04-10 10:57:46 +0400

    Added lots of stuff

---
+++ test/OGLContext.cpp
@@ -0,0 +1,9 @@
+//
+//  OGLContext.cpp
+//  test
+//
+//  Created by Dmitry Kozlov on 29.12.12.
+//  Copyright (c) 2012 Dmitry Kozlov. All rights reserved.
+//
+
+#include "OGLContext.h"
---
+++ test/OGLContext.h
@@ -0,0 +1,14 @@
+//
+//  OGLContext.h
+//  test
+//
+//  Created by Dmitry Kozlov on 29.12.12.
+//  Copyright (c) 2012 Dmitry Kozlov. All rights reserved.
+//
+
+#ifndef test_OGLContext_h
+#define test_OGLContext_h
+
+
+
+#endif
