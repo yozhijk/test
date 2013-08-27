63f852b1256f0d5a2a8cc691117b425c62a4879b
yozhijk <dmitry.a.kozlov@gmail.com>
2013-08-27 19:14:11 +0400

    Added some comments into ICamera.h

--- test/ICamera.h
+++ test/ICamera.h
@@ -11,6 +11,10 @@
 
 #include "IGameEntity.h"
 
+/////////////////////////////////
+///
+/// Basic camera interface
+/// 
 class ICamera : public IGameEntity
 {
 public:
@@ -19,9 +23,13 @@ public:
 	virtual core::matrix4x4 GetViewMatrix() const = 0;
 	virtual core::frustum   GetFrustum() const = 0;
 
+	/// Rotate camera aroung world up vector
 	virtual void Rotate(core::real angle) = 0;
+	/// Tilt in [-Pi/2,Pi/2] range
 	virtual void Tilt(core::real angle) = 0;
+	/// Move camera along current line of sight
 	virtual void MoveForward(core::real distance) = 0;
+	/// Set aspect ratio for the field of view
 	virtual void SetAspectRatio(core::real aspect) = 0;
 };
 
